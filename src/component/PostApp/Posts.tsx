5bca5633f3b27a38faf7dba5c144ae6af086e11b
Uffe Hellum (Zen3) <v-ufhel@microsoft.com>
2018-11-28 13:41:01 -0800

    cleanup state in redux

--- src/component/PostApp/Posts.tsx
+++ src/component/PostApp/Posts.tsx
@@ -55,7 +55,7 @@ class Posts extends Component<Props, State, any> {
   render = () =>
     <div>
       <h1>Posts</h1>
-      {this.props && this.props.posts && (this.props.posts as any).items && this.renderPosts((this.props.posts as any).items)}
+      {this.props && this.props.posts && this.renderPosts(this.props.posts)}
     </div>
 }
 
