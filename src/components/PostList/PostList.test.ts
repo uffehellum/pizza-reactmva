d32612882a404f1317be4e047fe8853310d969c7
Uffe Hellum (Zen3) <v-ufhel@microsoft.com>
2018-12-07 11:33:23 -0800

    ...

--- src/components/PostList/PostList.test.ts
+++ src/components/PostList/PostList.test.ts
@@ -1,30 +1,27 @@
 import { PostList } from './PostList'
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
 import Enzyme, { shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import { Post } from '../../types'
 
 describe('PostList without redux', () => {
-    beforeAll(() => {
-        Enzyme.configure({
-            adapter: new Adapter()
-        })
-    })
 
-
-    
-    it('componentWillMount calls fetchposts', () => {
-        let called = false
-        const fetchPosts: ()=>void = ()=> { called = true }
-        const posts: Post[] = []
-        const subject = new PostList({
-            fetchPosts,
-            posts,
-            propFromParent: 7
-        })
-        subject.componentWillMount()
-        expect(called).toBe(true)
+  beforeAll(() => {
+    Enzyme.configure({
+      adapter: new Adapter()
     })
+  })
+
+  it('componentWillMount calls fetchposts', () => {
+    let called = false
+    const fetchPosts: () => void = () => { called = true }
+    const posts: Post[] = []
+    const subject = new PostList({
+      fetchPosts,
+      posts,
+      propFromParent: 7
+    })
+    subject.componentWillMount()
+    expect(called).toBe(true)
+  })
+
 })
