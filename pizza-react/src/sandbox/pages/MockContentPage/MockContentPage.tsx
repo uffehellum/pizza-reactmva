d9b2307a7d95f44d55b35ce013df41892ddb24ea
Uffe Hellum (Zen3) <v-ufhel@microsoft.com>
2018-12-11 14:30:27 -0800

    mock content batch submitted when leaving page

--- pizza-react/src/sandbox/pages/MockContentPage/MockContentPage.tsx
+++ pizza-react/src/sandbox/pages/MockContentPage/MockContentPage.tsx
@@ -1,14 +1,19 @@
 import React, { Component } from 'react'
 import { connect, MapStateToProps } from 'react-redux';
-import { TelemetryEvent } from '../../../types'
-import { SaveTelemetryEventAsBatch } from '../../../redux/actions/telemetryActions'
+import { TelemetryEvent, Config } from '../../../types'
+import { SaveTelemetryEventAsBatch, SendBatchedTelemetryEvents} from '../../../redux/actions/telemetryActions'
 import EmbedHtmlPane from '../../../components/EmbedHtmlPane'
+import { CommandButton } from 'office-ui-fabric-react/lib/Button'
 
-interface StateProps { }
+interface StateProps {
+    config: Config
+}
+
 interface OwnProps { }
 
 interface DispatchProps {
     saveContentEvent: (header: string) => void
+    sendBatch: (telemetryEvent: TelemetryEvent, config: Config) => void
 }
 
 interface ContentBlock {
@@ -24,6 +29,20 @@ type Props = StateProps & OwnProps & DispatchProps
 
 export class MockContentPage extends Component<Props, State> {
 
+    constructor(props: Props) {
+        super(props)
+        this.state = {
+            contentBlocks: [
+                { header: 'cat', body: '<h3>cat</h3>' }
+            ]
+        }
+    }
+
+    showSquirrel = () =>
+        this.setState({
+            contentBlocks: [{ header: 'squirrel', body: 'Bushy Tail' }]
+        })
+
     renderContentBlock = (block: ContentBlock) => {
         this.props.saveContentEvent(block.header)
         return <EmbedHtmlPane header={block.header} html={block.body} />
@@ -35,13 +54,18 @@ export class MockContentPage extends Component<Props, State> {
             {blocks.map(this.renderContentBlock)}
         </div>
 
-    render = () => 
-        this.renderBlocks([
-            { header: 'cat', body: '<h3>cat</h3>' }
-        ])
+    render = () =>
+        <div>
+            <CommandButton onClick={this.showSquirrel}>Show Squirrel</CommandButton>
+            {this.renderBlocks(this.state.contentBlocks)}
+        </div>
 
-    componentWillUnmount = () => console.log("componentWillUnmount")
-    componentWillMount =() => console.log("ComponentWillMount")
+    componentWillUnmount = () => this.props.sendBatch(
+        {event:'leaving mock content page', payload:null, session: 'mock session', timestamp:new Date()},
+        this.props.config
+    )
+    
+    componentWillMount = () => console.log("ComponentWillMount")
 }
 
 function mapDispatchToProps(dispatch: any, ownProps: OwnProps): DispatchProps {
@@ -49,22 +73,28 @@ function mapDispatchToProps(dispatch: any, ownProps: OwnProps): DispatchProps {
         ...ownProps,
         saveContentEvent: (header: string) => {
             let telemetryEvent: TelemetryEvent = {
-                event : "content shown",
-                payload: {header},
+                event: "content shown",
+                payload: { header },
                 timestamp: new Date(),
                 session: "session",
             }
             SaveTelemetryEventAsBatch(telemetryEvent)(dispatch)
-        }
+        },
+        sendBatch: (telemetryEvent: TelemetryEvent, config: any) =>
+        SendBatchedTelemetryEvents(telemetryEvent, config)(dispatch)
+
     }
 }
 
-interface myReduxState { }
+interface myReduxState {
+    config: Config
+ }
 
 const mapStateToProps: MapStateToProps<StateProps, OwnProps, myReduxState> =
     (state: myReduxState, ownProps: OwnProps) => {
         return {
             ...ownProps,
+            config: state.config
         }
     }
 
