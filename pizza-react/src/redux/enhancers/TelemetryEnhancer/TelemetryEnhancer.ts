62e69da33c33064c17ecdac2b4bc6082a3dc5b36
Uffe Hellum (Zen3) <v-ufhel@microsoft.com>
2018-12-12 12:27:10 -0800

    telemetry enhancer leeches off other events

--- pizza-react/src/redux/enhancers/TelemetryEnhancer/TelemetryEnhancer.ts
+++ pizza-react/src/redux/enhancers/TelemetryEnhancer/TelemetryEnhancer.ts
@@ -1,9 +1,26 @@
 import { Dispatch, Store, Action, Middleware, MiddlewareAPI, AnyAction } from 'redux'
+import { SaveTelemetryEventAsBatch, SendBatchedTelemetryEvents, SendTelemetryEvent } from '../../actions/telemetryActions'
+import { ActionTypes, TelemetryEvent } from '../../../types';
+
+export async function Send(api:MiddlewareAPI<any>,request:TelemetryEvent ) {
+    const config = api.getState().config
+    SendTelemetryEvent(request, config)(api.dispatch)
+}
 
 const TelemetryEnhancer: Middleware =
     (api: MiddlewareAPI<any>) =>
         (nextDispatch: Dispatch<AnyAction>) =>
             (action: AnyAction) => {
+                switch (action.type) {
+                    case ActionTypes.FETCH_POSTS:
+                    Send(api, {
+                        event:"fetchposts",
+                        payload: '{}',
+                        session: 'enhancer session',
+                        timestamp: new Date(),
+                    })
+
+                }
                 nextDispatch(action)
             }
 
